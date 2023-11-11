3a2d660b1092db932150299dfbd695986ad7293b
GwenOHara <gmusker@hotmail.com>
2023-11-11 12:41:18 +0000

    Updating code style to move more to functions

--- Functions.R
+++ Functions.R
@@ -1,31 +1,48 @@
+####Fixed Variables Premium Bonds ####
+# Determine the current month 
+kdate <- tolower(format(Sys.Date(), "%B-%Y"))
+# Determine the url based on this month
+knewFile <- paste0("https://www.nsandi.com/files/asset/xlsx/prize-", kdate, ".xlsx")
 
-#### Functions #####
+#### Fixed Variables Share Price ####
 
-CreateAllBonds <- function(first.bond, second.bond, owner){
-  #Get only the numbers
-  end.first <- substr(first.bond, 6, 11)
-  end.second <- substr(second.bond, 6, 11)
-  start.first <- substr(first.bond, 1, 5)
-  interpolate.bonds <- seq.int(from = end.first, to = end.second)
-  full.bond.names <- data.table(bond = paste0(start.first, interpolate.bonds),
-                                owner = owner)
-  
-}
+kbenchmarks <- c("^FTSE","^GSPC")
+
+kftse100 <- GetFTSE100Stocks(
+  do.cache = TRUE,
+  cache.folder = file.path(tempdir(), "BGS_Cache"))
+
+ksp500 <- GetSP500Stocks(
+  do.cache = TRUE,
+  cache.folder = file.path(tempdir(), "BGS_Cache"))
 
 
-MakeBondNumbers <- function(bonds){
-  all.bonds <- rbindlist(Map(CreateAllBonds,
-                             first.bond = bonds$from, 
-                             second.bond = bonds$to, 
-                             owner = bonds$owner))
-}
 
-CheckPrizeFileExists <- function(newFile) {
-  out <- tryCatch(read.xlsx(newFile, startRow = 3), 
-                  error = function(e)
-                    NULL)
-  return(out)
-}
+kftse100tickers <- paste0(kftse100$tickers, ".L")
+ksp500tickers.data <- data.table(ksp500)[Tickers %in% c("AMZN", "AAPL", "MSFT", 
+                                                       "TSLA", "GOOGL", "GOOG", "NVDA", 
+                                                       "META", "BRK.B", "PFE", "KO" ,
+                                                       "UNH")]
+
+####Fixed Variables Retirement Planner ####
+
+klife_exp <- read.csv("https://raw.githubusercontent.com/GwenOHara/Personal_Finance_Assistant/retirement_planner/data/life_expectancy.csv",
+                     check.names = FALSE)
+klife_exp2 <- melt(data.table(klife_exp), id.vars = c('sex'))
+kassumptions <- read.csv("https://raw.githubusercontent.com/GwenOHara/Personal_Finance_Assistant/retirement_planner/data/retirement_forecast_assumptions.csv")
+kr_stk <- kassumptions$global_stocks
+kr_bond <- kassumptions$global_bonds
+kr_infl <- kassumptions$inflation
+kr_cash <- kassumptions$cash
+kexpected.returns  <- data.table(low = (90*kr_stk+10*kr_bond)/100, #0%-20% equities
+                                low_med = (70*kr_stk+30*kr_bond)/100, #20%-40% equities 
+                                med = (50*kr_stk+50*kr_bond)/100, #40%-60% equities
+                                med_high = (30*kr_stk+70*kr_bond)/100, #60%-80% equities,
+                                high = (10*kr_stk+90*kr_bond)/100) #80%-100% equities 
+kreal.expected.returns <- kexpected.returns[, lapply(.SD, function(col) col - kr_infl), 
+                                          .SDcols = colnames(kexpected.returns)]
+
+#### General Functions #####
 
 #Change a specific data table column of numbers to be formatted with a thousand separator
 ColNumThousandFormat <- function(dt, col){
@@ -126,19 +143,214 @@ ShowHideTabs <- function(inputID, tabs.to.hide, hide.flag){
 }
 
 
+#### Premium Bond Functions ####
+
+
+CreateAllBonds <- function(first.bond, second.bond, owner){
+  #Get only the numbers
+  end.first <- substr(first.bond, 6, 11)
+  end.second <- substr(second.bond, 6, 11)
+  start.first <- substr(first.bond, 1, 5)
+  interpolate.bonds <- seq.int(from = end.first, to = end.second)
+  full.bond.names <- data.table(bond = paste0(start.first, interpolate.bonds),
+                                owner = owner)
+  
+}
+
+
+MakeBondNumbers <- function(bonds){
+  all.bonds <- rbindlist(Map(CreateAllBonds,
+                             first.bond = bonds$from, 
+                             second.bond = bonds$to, 
+                             owner = bonds$owner))
+}
+
+CheckPrizeFileExists <- function(newFile) {
+  out <- tryCatch(read.xlsx(newFile, startRow = 3), 
+                  error = function(e)
+                    NULL)
+  return(out)
+}
+
+GetPrizeData <- function (newFile){
+  options(scipen = 999)
+  
+  prem.bond.prize.data <- data.table(read.xlsx(newFile, startRow = 3)) 
+  prem.bond.prize.data <- ColNumThousandFormat(prem.bond.prize.data, 'Val.of.Bond')
+  prem.bond.prize.data <- ColNumThousandFormat(prem.bond.prize.data, 'Prize.Value')
+  prem.bond.prize.data <- ColNumThousandFormat(prem.bond.prize.data, 'Total.V.of.Holding')
+  setnames(prem.bond.prize.data, 
+           c("Prize Value", "Bond Number", "Total Holding", "Area", "Bond value", "Date of Purchase"))
+  prem.bond.prize.data[, `Date of Purchase` := format(as.POSIXct(as.Date(`Date of Purchase`, 
+                                                                         origin = "1900-01-01")), '%b-%y')]
+  prem.bond.prize.data$Area <- as.factor(prem.bond.prize.data$Area)
+  
+  return(prem.bond.prize.data)
+  
+}
+
+
+MakeMatchTable <- function(output, prem.bonds.prize.data, all.bonds){
+  output$successful.message <- renderText(("There is a match in the high value winners this month!"))
+  
+  table <- data.table(prem.bonds.prize.data)
+  match.table <- table[, match := lapply(`Bond Number`, function(x) x %in% all.bonds$bonds)]
+  filter.match.table <-  match.table[`match` == "TRUE"][, `match` := NULL]
+  
+  setkey(filter.match.table, `Bond Number`)
+  setkey(all.bonds, `bonds`)
+  out <- filter.match.table[all.bonds]
+}
+
+#### Share Price Tracker ####
+
+
+GetAllTickers <- function(ftse100tickers, sp500tickers){
+  sp500tickers <- sp500tickers$Tickers
+  sp500tickers <- gsub("[.]B",'-B',sp500tickers)
+  alltickers <- c(ftse100tickers, sp500tickers)
+  
+}
+
+
+LoadStockData <- function(rv, session){
+  time.this({
+    rv$prices <- tq_get(rv$alltickers, 
+                        get  = "stock.prices",
+                        from = today()-months(60),
+                        to   = today(),
+                        complete_cases = F) %>%
+      select(symbol,date, close)
+    
+    rv$bench <- tq_get(kbenchmarks,
+                       get  = "stock.prices",
+                       from = today()-months(60),
+                       to   = today()) %>%
+      select(symbol, date, close)
+  }, "Updating share price information...", progress.bar = TRUE)
+  
+  join.ftse.data <- data.table(company = rv$ftse100$company,
+                               tickers = rv$ftse100$tickers,
+                               sector = rv$ftse100$ICB.sector,
+                               symbol = paste0(rv$ftse100$tickers, ".L"))
+  join.sp500.data <- data.table(company = rv$sp500$Company,
+                                tickers = rv$sp500$Tickers,
+                                sector = rv$sp500$GICS.Sector,
+                                symbol = rv$sp500$Tickers)
+  join.data <- rbind(join.ftse.data, join.sp500.data)
+  
+  rv$graph.data <- join.data[data.table(rv$prices), on = .(symbol)]
+  
+  updatePickerInput(session, 'stocks', 
+                    choices = sort(unique(rv$graph.data$company)), 
+                    selected = "Aviva")
+  rv$stocks <- TRUE
+  
+}
+
+Shares_Calculate_Data_For_Graphs <- function(rv, input){
+  prices.2 <- data.table(symbol = rv$graph.data$company,
+                         date = rv$graph.data$date ,
+                         close = rv$graph.data$close)
+  
+  prices <- prices.2[symbol %in% if(is.null(input$stocks))"AVvia" else input$stocks]
+  
+  if (input$period == 1) {
+    prices <- prices[date  >= today()-months(1)]
+  }
+  
+  if (input$period == 2) {
+    prices <- prices[date  >= today()-months(3)]
+  }
+  
+  if (input$period == 3) {
+    prices <- prices[date  >= today()-months(6)]}
+  
+  if (input$period == 4) {
+    prices <- prices[date  >= today()-months(12)]}
+  
+  if (input$period == 5) {
+    prices <- prices[date  >= today()-months(60)]}
+  
+  if (input$period == 6) {
+    prices <- prices[year(date)  == year(today())]}
+  
+  #For adding benchmark to data
+  bench2 <- data.table(rv$bench)
+  
+  if (input$benchmark == 1) {
+    bench <- bench2[symbol=="^GSPC" & date > min(prices$date)]
+    prices <- rbind(prices,bench) }
+  
+  if (input$benchmark == 2) {
+    bench <- bench2[symbol=="^FTSE" & date > min(prices$date)]
+    prices <- rbind(prices,bench) }
+  
+  return(prices)
+}
+
+
+SharePriceGraph1 <- function(prices){
+  ggplotly(prices %>%
+             ggplot(aes(date, close,colour = symbol)) +
+             geom_line(linewidth = 1, alpha = .9) +
+             # uncomment the line below to show area under curves
+             #geom_area(aes(fill=symbol),position="identity",alpha=.2) +
+             theme_minimal(base_size=16) +
+             theme(axis.title=element_blank(),
+                   plot.background = element_rect(fill = "black"),
+                   panel.background = element_rect(fill="black"),
+                   panel.grid = element_blank(),
+                   legend.text = element_text(colour="white"))
+  )
+}
+
+SharePriceGraph2 <-function(prices){
+  ggplotly(prices %>%
+             group_by(symbol) %>%
+             mutate(init_close = if_else(date == min(date),close,NA_real_)) %>%
+             mutate(value = round(100 * close / sum(init_close,na.rm=T),1)) %>%
+             ungroup() %>%
+             ggplot(aes(date, value,colour = symbol)) +
+             geom_line(linewidth = 1, alpha = .9) +
+             # uncomment the line below to show area under curves
+             #geom_area(aes(fill=symbol),position="identity",alpha=.2) +
+             theme_minimal(base_size=16) +
+             theme(axis.title=element_blank(),
+                   plot.background = element_rect(fill = "black"),
+                   panel.background = element_rect(fill="black"),
+                   panel.grid = element_blank(),
+                   legend.text = element_text(colour="white"))
+  )
+}
+
+SharePriceTable <- function(prices){
+  current.share.price <-
+    if( nrow(prices[date == today()]) == 0){
+      prices[date == today()-1]
+    } else{
+      prices[date == today()]}
+  
+  current.share.price[ , close := format(round(current.share.price$close, 2), big.mark = ",")]
+  setnames(current.share.price, "symbol", "company")
+  
+  return(current.share.price)
+  
+}
+
 #### Retirement Forecast ####
 
-Life_Expectancy <- function(age, gender, life_exp){
+Life_Expectancy <- function(age, gender, life_exp, life_exp2){
 current.year <- as.numeric(format(Sys.Date(), "%Y"))
 birth.year <- current.year - age
-life_exp2 <- melt(data.table(life_exp), id.vars = c('sex'))
 birth.year.check <- if(birth.year < min(as.numeric(colnames(life_exp[2:ncol(life_exp)])))){
   min(as.numeric(colnames(life_exp[2:ncol(life_exp)])))
 } else {
   birth.year
 }
 
-life.exp <- life_exp2[variable == birth.year.check, ][, year.left := value - age]
+life.exp <- life_exp2[variable == birth.year.check, ][, years.to.exp := round({value - age},0)]
+life.exp[, years.to.max := 120 - age][, value := round(value,0)]
 out <- life.exp[ sex == gender]
 
 }
--- server.R
+++ server.R
@@ -16,12 +16,9 @@ shinyServer(function(input, output, session = getDefaultReactiveDomain()) {
   # Init ===============================================================
   
   rv <- reactiveValues()
-  
-  
   output$style_tag <- renderUI({
     if(input$Tabs=='Tab1')
       return(tags$head(tags$style(HTML('.content-wrapper {background-color:#a7d6d4;}'))))
-    
 
     if(input$Tabs=='Tab2')
       return(tags$head(tags$style(HTML('.content-wrapper {background-color:a7d6d4;}'))))
@@ -35,40 +32,18 @@ shinyServer(function(input, output, session = getDefaultReactiveDomain()) {
   
   # High Value Premium Bond Prize Checker #### ===============================================================
   
-  
-  # Determine the current month 
-  date <- tolower(format(Sys.Date(), "%B-%Y"))
-  
   output$title <- renderText(paste("NS&I Premium Bonds High Value Prize Checker", format(Sys.Date(), "%B %Y")))
   
-  # Determine the url based on this month
-  newFile <- paste0("https://www.nsandi.com/files/asset/xlsx/prize-", date, ".xlsx")
-  # Create filename to save the data to system folder
-  file_name <- paste0("prize-", date, ".csv")
-  file_path <- paste0(getwd(),"/data/")
-  file <- paste0(file_path, file_name)
+ 
+  #Check if the file exists on NS&I website 
+  check.data <- CheckPrizeFileExists(newFile = knewFile)
   
-  #Check if the file existis on NS&I website 
-  check.data <- CheckPrizeFileExists(newFile = newFile)
+  #Output the prize data
   if(is.null(check.data)){
     shinyalert("File not found", "Current months file is not available on NS&I website", type = "error")
   } else {
-    
-    
-    #Getting prize data  
-    options(scipen = 999)
-    
-    prem.bond.prize.data <- data.table(read.xlsx(newFile, startRow = 3)) 
-    prem.bond.prize.data <- ColNumThousandFormat(prem.bond.prize.data, 'Val.of.Bond')
-    prem.bond.prize.data <- ColNumThousandFormat(prem.bond.prize.data, 'Prize.Value')
-    prem.bond.prize.data <- ColNumThousandFormat(prem.bond.prize.data, 'Total.V.of.Holding')
-    setnames(prem.bond.prize.data, 
-             c("Prize Value", "Bond Number", "Total Holding", "Area", "Bond value", "Date of Purchase"))
-    prem.bond.prize.data[, `Date of Purchase` := format(as.POSIXct(as.Date(`Date of Purchase`, 
-                                                                           origin = "1900-01-01")), '%b-%y')]
-    prem.bond.prize.data$Area <- as.factor(prem.bond.prize.data$Area)
-    rv$prem.bonds.prize.data <- prem.bond.prize.data
-
+    #Getting prize data 
+    rv$prem.bonds.prize.data <- prem.bond.prize.data <- GetPrizeData(newFile = knewFile)
     output$pbtable <- renderDT(prem.bond.prize.data, rownames =FALSE, 
                                filter = "top",
                                options = list(
@@ -85,25 +60,22 @@ shinyServer(function(input, output, session = getDefaultReactiveDomain()) {
       actionButton('data.example', 'See example data format for csv file', icon = icon("table"),
                    style = "color: #fff; background-color: #52606D; border-color: #3e4c59"))
     
-    
     output$file.input <- renderUI(
       fileInput("premium.bonds", "Upload CSV file of your Premium Bonds",
                 accept = c(
                   ".csv"))) 
     
-    observeEvent(input$data.example, {
-      toggleModal(session, 'modalExample', toggle = "open")
-    })
-    
-    
     output$csv.data <- DT::renderDT(
       datatable(data.table(from = c('bond number 1','bond number 3'),
                            to = c('bond number 2','bond number 4'),
-                           owner = c('name i.e. pingu', 'name i.e. gromit')), rownames = FALSE),
-      
-      
-    )
+                           owner = c('name i.e. pingu', 'name i.e. gromit')), rownames = FALSE))
     
+  } # end of else after if check.data = NULL
+  
+  
+    observeEvent(input$data.example, {
+      toggleModal(session, 'modalExample', toggle = "open")
+    })
     
     observeEvent(input$bondchecker,{
       if(is.null(input$premium.bonds)) 
@@ -116,16 +88,9 @@ shinyServer(function(input, output, session = getDefaultReactiveDomain()) {
       all.bonds <- MakeBondNumbers(bonds = bonds)
       
       if(any(rv$prem.bonds.prize.data$`Bond Number` %in% all.bonds$bonds)){
-        output$successful.message <- renderText(("There is a match in the high value winners this month!"))
-        
-        table <- data.table(rv$prem.bonds.prize.data)
-        match.table <- table[, match := lapply(`Bond Number`, function(x) x %in% all.bonds$bonds)]
-        filter.match.table <-  match.table[`match` == "TRUE"][, `match` := NULL]
-        
-        
-        setkey(filter.match.table, `Bond Number`)
-        setkey(all.bonds, `bonds`)
-        filter.match.table[all.bonds]
+        filter.match.table <- MakeMatchTable(output = output, 
+                                             prem.bonds.prize.data = rv$prem.bonds.prize.data, 
+                                             all.bonds = all.bonds)
         
         output$prize.matches <- renderDT(datatable(filter.match.table, rownames =FALSE), 
                                          filter = "top",
@@ -140,157 +105,38 @@ shinyServer(function(input, output, session = getDefaultReactiveDomain()) {
       }
     })
     
-  } # end of else after if check.data = NULL
+  #} 
   
   # Share Price Tracker #### ===============================================================
-  rv$ftse100 <- ftse100 <- GetFTSE100Stocks(
-         do.cache = TRUE,
-         cache.folder = file.path(tempdir(), "BGS_Cache"))
   
-  rv$sp500 <- sp500 <- GetSP500Stocks(
-    do.cache = TRUE,
-    cache.folder = file.path(tempdir(), "BGS_Cache"))
-  
-  benchmarks <- c("^FTSE","^GSPC")
-
-  ftse100tickers <- paste0(ftse100$tickers, ".L")
-  sp500tickers.data <- data.table(sp500)[Tickers %in% c("AMZN", "AAPL", "MSFT", 
-                                                        "TSLA", "GOOGL", "GOOG", "NVDA", 
-                                                        "META", "BRK.B", "PFE", "KO" ,
-                                                        "UNH")]
-  sp500tickers <- sp500tickers.data$Tickers
-  sp500tickers <- gsub("[.]B",'-B',sp500tickers)
-  alltickers <- c(ftse100tickers, sp500tickers)
+  rv$ftse100 <- kftse100
+  rv$sp500 <- ksp500 
   rv$stocks <- NULL
-  
-  observeEvent(input$Tabs,{
-    if(input$Tabs == "Tab2"){
-      
-      time.this({
-        rv$prices <- tq_get(alltickers, 
-                         get  = "stock.prices",
-                         from = today()-months(60),
-                         to   = today(),
-                         complete_cases = F) %>%
-          select(symbol,date,close)
-        
-        rv$bench <- tq_get(benchmarks,
-                        get  = "stock.prices",
-                        from = today()-months(60),
-                        to   = today()) %>%
-          select(symbol,date,close)
-     }, "Updating share price information...", progress.bar = TRUE)
-      
-      join.ftse.data <- data.table(company = rv$ftse100$company,
-                                    tickers = rv$ftse100$tickers,
-                                    sector = rv$ftse100$ICB.sector,
-                                   symbol = paste0(rv$ftse100$tickers, ".L"))
-      join.sp500.data <- data.table(company = rv$sp500$Company,
-                                   tickers = rv$sp500$Tickers,
-                                   sector = rv$sp500$GICS.Sector,
-                                   symbol = rv$sp500$Tickers)
-      join.data <- rbind(join.ftse.data, join.sp500.data)
-      
-      rv$graph.data <- join.data[data.table(rv$prices), on = .(symbol)]
-            
-      updatePickerInput(session, 'stocks', 
-                        choices = sort(unique(rv$graph.data$company)), 
-                        selected = "Aviva")
-      rv$stocks <- TRUE
-    }
+ 
+   observe({
+    req(kftse100tickers, ksp500tickers.data)
+    rv$alltickers <- GetAllTickers(kftse100tickers, ksp500tickers.data)
   })
   
+  observeEvent(input$Tabs,{
+    if(input$Tabs == "Tab2")LoadStockData(rv = rv, session = session)
+  })
   
-  # server logic based on user input
   observeEvent(c(input$period,input$stocks,input$benchmark), {
    req(!is.null(rv$stocks))
-   
-    prices.2 <- data.table(symbol = rv$graph.data$company,
-                           date = rv$graph.data$date ,
-                           close = rv$graph.data$close)
+    prices <- Shares_Calculate_Data_For_Graphs(rv, input)
 
-    prices <- prices.2[symbol %in% if(is.null(input$stocks))"AVvia" else input$stocks]
-
-    if (input$period == 1) {
-      prices <- prices[date  >= today()-months(1)]
-    }
-    
-    if (input$period == 2) {
-      prices <- prices[date  >= today()-months(3)]
-     }
-    
-    if (input$period == 3) {
-      prices <- prices[date  >= today()-months(6)]}
-    
-    if (input$period == 4) {
-      prices <- prices[date  >= today()-months(12)]}
-    
-    if (input$period == 5) {
-      prices <- prices[date  >= today()-months(60)]}
-    
-    if (input$period == 6) {
-      prices <- prices[year(date)  == year(today())]}
-   
-    bench2 <- data.table(rv$bench)
-    
-    if (input$benchmark == 1) {
-      bench <- bench2[symbol=="^GSPC" & date > min(prices$date)]
-     
-      prices <- rbind(prices,bench) }
-    
-    if (input$benchmark == 2) {
-      bench <- bench2[symbol=="^FTSE" & date > min(prices$date)]
-      prices <- rbind(prices,bench) }
-    
-    # Create plot
+  # Create plot
     output$share.price.plot <- renderPlotly({
-        ggplotly(prices %>%
-                   ggplot(aes(date, close,colour = symbol)) +
-                   geom_line(size = 1, alpha = .9) +
-                   # uncomment the line below to show area under curves
-                   #geom_area(aes(fill=symbol),position="identity",alpha=.2) +
-                   theme_minimal(base_size=16) +
-                   theme(axis.title=element_blank(),
-                         plot.background = element_rect(fill = "black"),
-                         panel.background = element_rect(fill="black"),
-                         panel.grid = element_blank(),
-                         legend.text = element_text(colour="white"))
-        )
+      SharePriceGraph1(prices)
     })
     
-
-
-
-
     output$relative.share.price.plot <- renderPlotly({
-        ggplotly(prices %>%
-                   group_by(symbol) %>%
-                   mutate(init_close = if_else(date == min(date),close,NA_real_)) %>%
-                   mutate(value = round(100 * close / sum(init_close,na.rm=T),1)) %>%
-                   ungroup() %>%
-                   ggplot(aes(date, value,colour = symbol)) +
-                   geom_line(size = 1, alpha = .9) +
-                   # uncomment the line below to show area under curves
-                   #geom_area(aes(fill=symbol),position="identity",alpha=.2) +
-                   theme_minimal(base_size=16) +
-                   theme(axis.title=element_blank(),
-                         plot.background = element_rect(fill = "black"),
-                         panel.background = element_rect(fill="black"),
-                         panel.grid = element_blank(),
-                         legend.text = element_text(colour="white"))
-      )
-    })
+      SharePriceGraph2(prices)
+      })
     
-    current.share.price <-
-      if( nrow(prices[date == today()]) == 0){
-        prices[date == today()-1]
-      } else{
-      prices[date == today()]}
-
-    current.share.price[ , close := format(round(current.share.price$close, 2), big.mark = ",")]
-    setnames(current.share.price, "symbol", "company")
-    
-    output$share.price.table <- renderDT(datatable(current.share.price, rownames =FALSE), 
+    output$share.price.table <- renderDT(datatable(SharePriceTable(prices), 
+                                                   rownames =FALSE), 
                                          )
   })
 
@@ -299,21 +145,6 @@ shinyServer(function(input, output, session = getDefaultReactiveDomain()) {
   #https://advisors.vanguard.com/insights/article/series/market-perspectives#projected-returns
   
   #https://www.ons.gov.uk/peoplepopulationandcommunity/birthsdeathsandmarriages/lifeexpectancies/datasets/expectationoflifeprincipalprojectionunitedkingdom
-  
-  life_exp <- read.csv("https://raw.githubusercontent.com/GwenOHara/Personal_Finance_Assistant/retirement_planner/data/life_expectancy.csv",
-                       check.names = FALSE)
-  assumptions <- read.csv("https://raw.githubusercontent.com/GwenOHara/Personal_Finance_Assistant/retirement_planner/data/retirement_forecast_assumptions.csv")
-  r_stk <- assumptions$global_stocks
-  r_bond <- assumptions$global_bonds
-  r_infl <- assumptions$inflation
-  r_cash <- assumptions$cash
-  expected.returns  <- data.table(low = (90*r_stk+10*r_bond)/100, #0%-20% equities
-                                              low_med = (70*r_stk+30*r_bond)/100, #20%-40% equities 
-                                              med = (50*r_stk+50*r_bond)/100, #40%-60% equities
-                                              med_high = (30*r_stk+70*r_bond)/100, #60%-80% equities,
-                                              high = (10*r_stk+90*r_bond)/100) #80%-100% equities 
-  real.expected.returns <- expected.returns[, lapply(.SD, function(col) col - r_infl), 
-                                       .SDcols = colnames(expected.returns)]
   
   
   # When calculating  what income is needed in retirement use Department of Work and Pensions guidelines of:
@@ -330,13 +161,20 @@ shinyServer(function(input, output, session = getDefaultReactiveDomain()) {
   
   
   observeEvent({input$gender; input$age}, {
-    req(life_exp)
+    req(klife_exp, klife_exp2)
     rv$life.exp.gend <- Life_Expectancy(age = input$age, 
                                       gender = input$gender, 
-                                      life_exp = life_exp)
+                                      life_exp = klife_exp,
+                                      life_exp2 = klife_exp2)
   })
   
 
+  observeEvent({input$current.income; input$pension.pot; input$contribution; 
+    input$empl.contribution; input$cash.not.isa; input$cash; input$lisa; input$s_and_s;
+    input$other},{
+      req(rv$life.exp.gend)
+    })
+  
   #rv$retirement.forecast <- data.table( years = c(1:round(rv$life.exp$year.left,0)))  
   
   
